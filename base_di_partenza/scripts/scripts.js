6b20b161de9ba1d359f9d439371153a6625d1727
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2025-10-24 16:35:17 +0200

    animation set

--- base_di_partenza/scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-// const hidden = document.getElementsByClassName('hidden')
-
-// if (screen.availWidth> 1024) {
-//     for (i = 0; i < hidden.length, i++;)
-//     {
-//         hidden[i].setAttribute('class', '')
-//     }
-// }
